913aaa0b8f3e3aadfbaf94db1decb3efd01aecab
eternalsyf <3010693001@qq.com>
2022-03-02 07:40:00 +0800

    Initial commit

---
+++ src/main/java/com/syf/demo1/controller/MyTestController.java
@@ -0,0 +1,16 @@
+package com.syf.demo1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/myTest")
+public class MyTestController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+
+}
